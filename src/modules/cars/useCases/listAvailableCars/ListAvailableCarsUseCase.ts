e8fb1f1b8d3854ab8290c864af57e05b0dd85d7d
Lucas Weslen <matoslopes23@gmail.com>
2021-07-08 18:23:34 -0400

    Create ListAvailableCarsController

--- src/modules/cars/useCases/listAvailableCars/ListAvailableCarsUseCase.ts
+++ src/modules/cars/useCases/listAvailableCars/ListAvailableCarsUseCase.ts
@@ -1,5 +1,6 @@
 import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { inject, injectable } from "tsyringe";
 
 
 interface IRequest {
@@ -7,9 +8,10 @@ interface IRequest {
     brand?: string;
     name?: string;
 }
-
+@injectable()
 class ListAvailableCarsUseCase {
     constructor (
+        @inject("CarsRepository")
         private carsRepository: ICarsRepository
     ){}
     async execute({category_id, brand, name}:IRequest): Promise<Car[]>{
--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -3,11 +3,15 @@ import { Router } from "express";
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
 
 const carsRouter = Router();
 
-const createCarController = new CreateCarController()
+const createCarController = new CreateCarController();
+const listAvailableCarsController = new ListAvailableCarsController();
 
-carsRouter.post("/",ensureAuthenticated, ensureAdmin, createCarController.handle)
+carsRouter.post("/",ensureAuthenticated, ensureAdmin, createCarController.handle);
+
+carsRouter.get("/available", listAvailableCarsController.handle);
 
 export {carsRouter}

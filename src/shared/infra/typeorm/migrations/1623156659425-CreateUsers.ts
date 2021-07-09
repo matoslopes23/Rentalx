e223184b11408ad9a841c5459566955ebba5487c
Lucas Weslen <matoslopes23@gmail.com>
2021-07-09 12:35:12 -0400

    Create migration Rentals

--- src/shared/infra/typeorm/migrations/1623156659425-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1623156659425-CreateUsers.ts
@@ -10,6 +10,7 @@ export class CreateUsers1623156659425 implements MigrationInterface {
                     {
                         name:"id",
                         type:"uuid",
+                        isPrimary: true
                     },
                     {
                         name:"name",
